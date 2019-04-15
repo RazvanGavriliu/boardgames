98deb779d220612a81c55c39e0ed189856596123
razvan <razvan1.gavriliu@gmail.com>
2019-04-15 20:13:43 +0300

    dict parameter -> for message

--- main/views.py
+++ main/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-# def home(request):
-#     return HttpResponse("Hello, World")
 def home(request):
     return render(request, "main/home.html",{'message':'Hi, there!'})
+
